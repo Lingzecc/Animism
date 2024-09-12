82335d58da6917b34251bb5e27c7a236eff510a8
Lingze <13898650+lingze123@user.noreply.gitee.com>
2024-09-13 00:57:38 +0800

    口型音频同步和根据播放协议的更新

--- weblive2d.py
+++ weblive2d.py
@@ -2,7 +2,13 @@ from flask import Flask, send_from_directory, request, flash, redirect, send_fil
 import os
 import json
 import moviepy.video.io.ffmpeg_tools
-# from llm import chat
+from llm import chat
+from tools.listen import recognize_speech
+from tools.tts import tts
+import pygame
+import time
+import numpy as np
+import librosa
 app = Flask(__name__)
 
 # 网页投射
@@ -16,6 +22,9 @@ def web():
 @app.route('/assets/<path:path>')
 def live2d(path):
     return send_from_directory('./assets/',path)
+
+
+
 # 音频位置
 UPLOAD_FOLDER = 'data/record_audio/'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -23,14 +32,13 @@ if not os.path.exists(UPLOAD_FOLDER):
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-
 # 获取前端音频保存到UPLOAD_FOLDER
-@app.route('/upload', methods=['POST'])
+@app.route('/upload/audio', methods=['POST'])
 def record_audio2wav():
-    if 'audioFile' not in request.files:
+    if 'audioFormData' not in request.files:
         flash('没有文件部分')
         return redirect(request.url)
-    file = request.files['audioFile']
+    file = request.files['audioFormData']
     if file.filename == '':
         flash('没有选择文件')
         return redirect(request.url)
@@ -44,38 +52,61 @@ def record_audio2wav():
         print(f"音频已成功转换为WAV格式，并保存到{filepath}")
         return '文件上传成功'
 
+response_llm = ""
+
+@app.route('/upload/tts', methods=['POST'])
+def text_to_speech():
+    text = request.form['text']
+    # response_llm = chat(text)
+    # tts(response_llm, "127.0.0.1", "8000", tmp_audio_path='data/tts_output')
+    return send_file("D:/PORJECT/Animism/Allium/data/tts_output/tmp.wav", mimetype='audio/wav')
+    
 
 
 # 开口大小文件路由
 """
 引用自https://juejin.cn/post/7242279345136861241
 """
-@app.route('/api/mouth')
+@app.route('/api/mouth', methods=['GET'])
 def mouth():
     with open("data/tts_output/tmp.txt", "r") as f:
         return json.dumps({
             "y": f.read()
         })  
-        
-        
-        
-@app.route('/data/tts', methods=['GET'])  
-def get_audio_file():  
-    # 添加一个查询参数来绕过缓存，这里使用当前时间戳  
-    timestamp = request.args.get('timestamp', 0)  
-    # 确保timestamp是一个整数
-    timestamp = int(timestamp) if timestamp.isdigit() else 0  
-    print(f"/data/tts?{timestamp}")
-    # 返回音频文件  
-    return send_from_directory("data/tts_output/", 'tmp.wav', as_attachment=True)
 
 
-# @app.route('/data/llm', methods=['GET'])  
-# def get_text():
-#     text = chat()
-#     return json.dumps({
-#         "text": text
-#     })
+       
+@app.route('/mouthY', methods=['POST'])  
+def mouthY(tmp_audio_path='data/tts_output'):
+    pygame.mixer.init()
+    pygame.mixer.music.load(f"{tmp_audio_path}/tmp.wav")  
+    pygame.mixer.music.set_volume(0.8) 
+
+    x , sr = librosa.load(f"{tmp_audio_path}/tmp.wav", sr=8000)
+
+    x = x  - min(x)
+    x = x  / max(x)
+    x= np.log(x) + 1
+    x = x  / max(x) * 1.5
+    # 调用本机扬声器
+    # pygame.mixer.music.play()
+    s_time = time.time()
+    try:
+        for _ in range(int(len(x) / 800)):
+            it = x[int((time.time() - s_time) * 8000)+1]
+            # print(it)
+            if it < 0:
+                it = 0
+            with open(f"{tmp_audio_path}/tmp.txt", "w") as f:
+                f.write(str(float(it)))
+            time.sleep(0.1)
+    except:
+        pass
+
+    time.sleep(0.1)
+    with open(f"{tmp_audio_path}/tmp.txt", "w") as f:
+        f.write("0")
+
 
 
 
