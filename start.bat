1eab7c36a7a1e74f51725e14754a145af6dfaa48
Lingze <13898650+lingze123@user.noreply.gitee.com>
2024-09-26 08:59:26 +0800

    启动修补

--- start.bat
+++ start.bat
@@ -6,8 +6,7 @@ call conda activate %CONDA_ENV_NAME%
 echo 启动程序中.....
 echo 正在补妆...
 echo 神秘通道：http://127.0.0.1:4800/
-start cmd /k python "webtest.py"  
-echo Python 程序正在运行中的两个黑窗口不要关闭，此窗口可关闭。
+start cmd /k python "main.py"  
 echo （按任意键退出）聊的愉快Ciallo～ （∠・ω< ）⌒★
 :End  
 endlocal  
