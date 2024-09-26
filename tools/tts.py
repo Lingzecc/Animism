1ecc9abdfcbb9bcef8116a60c003000d716952fa
Lingze <13898650+lingze123@user.noreply.gitee.com>
2024-09-26 09:00:52 +0800

    添加本地与远程双选项

--- tools/tts.py
+++ tools/tts.py
@@ -30,26 +30,47 @@ if ref_text:
     ref_text = read_ref_text(ref_text)
 # 开启fish-speech的api后访问api转换返回语音
 def tts(text, host, port, tmp_audio_path='data/tts_output'):
-    url = f"http://{host}:{port}/v1/invoke"
+    # 本地访问
+    # url = f"http://{host}:{port}/v1/invoke"
+    # 远程访问
+    url = url = "https://api.fish.audio/v1/tts"
+    querystring = {"author_id":"1aacaeb1b840436391b835fd5513f4c4"}
+    headers = {
+    "Authorization": "Bearer f56f8b383e8a46e482e0213e942748ff",
+    "Content-Type": "application/json"
+}
+    
+    # data = {
+    #     "text": text,
+    #     "reference_text": ref_text,
+    #     "reference_audio": base64_audio,
+    #     "max_new_tokens": 1024,
+    #     "chunk_length": 100,
+    #     "top_p": 0.7,
+    #     "repetition_penalty": 1.2,
+    #     "temperature": 0.7,
+    #     "speaker": None,
+    #     "format": "wav",
+    #     "streaming": "False",
+    # }
+
     data = {
         "text": text,
-        "reference_text": ref_text,
-        "reference_audio": base64_audio,
+        "reference_id": "1aacaeb1b840436391b835fd5513f4c4",
+        "chunk_length": 200,
         "max_new_tokens": 1024,
-        "chunk_length": 100,
-        "top_p": 0.7,
-        "repetition_penalty": 1.2,
-        "temperature": 0.7,
-        "speaker": None,
-        "emotion": "happy",
+        "normalize": True,
         "format": "wav",
-        "streaming": "False",
+        "mp3_bitrate": 64,
+        "latency": "normal"
     }
-
-    response = requests.post(url, json=data)
+    
+    # response = requests.post(url, json=data)
+    response = requests.request("POST", url, json=data, headers=headers)
     
     if response.status_code == 200:
         audio_content = response.content
+        # audio_content = response.text
 
         with open(f"{tmp_audio_path}/tmp.wav", "wb") as audio_file:
             audio_file.write(audio_content)
